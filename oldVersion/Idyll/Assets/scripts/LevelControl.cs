59e6b711ea17b6cb4dab7bf30c2c2a58e37d2300
eabosch <hedy.carroll@gmail.com>
2019-03-07 12:40:50 -0800

    old version of the unity file

---
+++ oldVersion/Idyll/Assets/scripts/LevelControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelControl : MonoBehaviour
+{
+	public string levelname;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	/*void OnTriggerEnter(Collider2D other)
+	{
+		if (other.CompareTag ("Player")) 
+		{
+			UnityEngine.SceneManagement.SceneManager.LoadScene(levelname);
+		}
+	}*/
+}
